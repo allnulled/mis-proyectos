c2725e8e10a1c234f3c7040fdc6fefb954a5567c
allnulled <todosanulados@gmail.com>
2024-12-28 06:55:32 +0100

    orden inverso a como los presenta NPM por defecto, que es de viejo a nuevo, pues de nuevo a viejo, algo simple, algo que si ha pillado github, y que npm por ser de la misma compañia cabria pensar que tambien pillan porque npm tambien es bastante critico y lo lleva microsoft, no lo se

--- build.js
+++ build.js
@@ -1,9 +1,17 @@
 const fs = require("fs");
 const ejs = require("ejs");
 
+const reverseObjectKeys = function(obj) {
+  const reversedObj = {};
+  const keys = Object.keys(obj).reverse(); // Obtén las claves en orden inverso
+  keys.forEach(key => {
+    reversedObj[key] = obj[key]; // Añade las propiedades al nuevo objeto
+  });
+  return reversedObj;
+}
 
 const main = async function () {
-  const dataNpm = require(__dirname + "/npm.json");
+  const dataNpm = reverseObjectKeys(require(__dirname + "/npm.json"));
   const output = await ejs.renderFile(__dirname + "/README.ejs.md", { data: dataNpm });
   fs.writeFileSync(__dirname + "/README.md", output, "utf8");
 };
